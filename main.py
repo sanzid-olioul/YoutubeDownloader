23b0195694afd1af2b408d90993ecce1b3589cc8
Sanzid <sanzid94@gmail.com>
2023-05-15 08:05:33 +0600

    Bug fixed

--- main.py
+++ main.py
@@ -16,26 +16,34 @@ class YoutubeDownloader:
         self.__root.resizable(False,False)
         self.__root.grid_columnconfigure(0, weight=1)
         self.__root.grid_rowconfigure(0, weight=1)
-        self._tab = ttk.Notebook(self.__root)
+        self._tab = ttk.Notebook(self.__root,height='580')
         self._tab.grid(sticky='wens')
-    
-    def _home_tab(self):
         s = ttk.Style()
         s.configure('TFrame', background='#0F969C')
+    
+    def _home_tab(self):
         home_image = tkinter.PhotoImage(file='images/home.png')
         home_tab = ttk.Frame(self._tab)
         self._tab.add(home_tab,text='Home',image=home_image,compound='left')
-        home = HomeTab(self._tab)
+        home = HomeTab(home_tab)
         home.add()
 
     def _downloading_tab(self):
-        pass
+        download_image = tkinter.PhotoImage(file='images/download.png')
+        download_tab = ttk.Frame(self._tab)
+        self._tab.add(download_tab,text='Download',image=download_image,compound='left')
+
 
     def _compleated_tab(self):
-        pass
+        complete_image = tkinter.PhotoImage(file='images/check.png')
+        complete_tab = ttk.Frame(self._tab)
+        self._tab.add(complete_tab,text='Completed',image=complete_image,compound='left')
+
 
     def download(self):
         self._home_tab()
+        self._downloading_tab()
+        self._compleated_tab()
         self.__root.mainloop()
 
 
